553b72f121414983a11629d0b64c44e1a4811f7e
alexBedolla <j.alexbedolla.a@gmail.com>
2022-11-23 19:04:02 -0700

    todo-list app init

---
+++ kataFrontend/todo-list/src/Pages/Home/Todo.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Todo.css";
+
+export default class Todo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-3xl text-sky-500">To-do List</h1>
+        </header>
+      </div>
+    );
+  }
+}
