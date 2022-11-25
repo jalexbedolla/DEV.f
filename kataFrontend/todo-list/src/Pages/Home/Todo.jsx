7dbb137a9841b0a61aef3e962a651ee7a14a0c67
alexBedolla <j.alexbedolla.a@gmail.com>
2022-11-24 21:57:58 -0700

    no jala

--- kataFrontend/todo-list/src/Pages/Home/Todo.jsx
+++ kataFrontend/todo-list/src/Pages/Home/Todo.jsx
@@ -10,35 +10,47 @@ export default class Todo extends Component {
     super(props); /* despues el super, que no me acuerdo para que es..!! */
     /* y este es el famoso estado */
     this.state = {
-      nuevaTarea: "",
-      lista: [],
+      newItem: "",
+      list: [],
     };
   }
 
   /* ahora vienen las malditas funciones */
-  agregarTarea() {
-    /* esta es para el boton 'add', create item with unique id */
-    const nuevaTarea = {
-      id: 1 + Math.random(),
-      value: this.state.nuevaTarea.slice(),
-    };
-    /* copiar la tarea del input */
-    const lista = [...this.state.lista];
-    /* agregar la tarea a la lista */
-    list.push(nuevaTarea);
-    /* actualizar el stado con la nueva tarea y limpiar el input */
-    this.setState({
-      lista,
-      nuevaTarea: "",
-    });
-  }
-
-  actualizarTarea(key, value) {
-    /* actualizar el estado */
+  updateInput(key, value) {
+    /* actualizar el estado de React */
     this.setState({
       [key]: value,
     });
   }
+
+  addItem() {
+    /* esto es para agregarle un id para poder borrarlo */
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+
+    /* tomar una copia de la lista de tareas */
+    const list = [...this.state.list];
+
+    /* agregar la tarea a la lista */
+    list.push(newItem);
+
+    /* actualizar el estado con la nueva tarea y limpiar el input */
+    this.setState({
+      list,
+      newItem: "",
+    });
+  }
+  /* funcion eliminar */
+  deleteItem(id) {
+    /* una copia de los articulos de la lista */
+    const list = [...this.state.list];
+    /* filtrar las tareas para ser borrada */
+    const updatedList = list.filter((item) => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="text-center mt-10">
@@ -49,18 +61,34 @@ export default class Todo extends Component {
           <input
             className="w-72 rounded-md px-3 py-3 bg-[#e8ecf4] backdrop-blur-lg"
             type="text"
-            onChange={(e) => this.actualizaTarea("nuevaTarea", e.target.value)}
-            placeholder="Agregar Tarea"
+            placeholder="Type a Task here..."
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput("newItem", e.target.value)}
           />
           <button
             className="h-full px-3 py-3 bg-orange-500 text-white font-medium rounded-md"
-            onClick={() => this.agregarTarea()}
+            onClick={() => this.addItem()}
           >
             add
           </button>
         </div>
         <div>
-          <ul>{}</ul>
+          <ul>
+            {this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+
+                  <button
+                    className="h-full px-3 py-1 bg-red-500 text-white font-medium rounded-md"
+                    onClick={() => this.deleteItem(item.id)}
+                  >
+                    X
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     );
