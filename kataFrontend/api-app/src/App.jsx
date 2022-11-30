6135b4b34f4fec4b27b8f1abdfdeb30dbcd0de09
alexBedolla <j.alexbedolla.a@gmail.com>
2022-11-30 08:41:21 -0700

    api project init

---
+++ kataFrontend/api-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+
+export default class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
