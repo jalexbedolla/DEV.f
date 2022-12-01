3c5cf68ee4459bd400ac0badb1f7b9c30affc9dc
alexBedolla <j.alexbedolla.a@gmail.com>
2022-12-01 14:10:25 -0700

    quien sabe que verga paso

--- kataFrontend/api-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import "./App.css";
-import Home from "./Pages/Home";
-
-export default class App extends Component {
-  render() {
-    return <Home />;
-  }
-}
