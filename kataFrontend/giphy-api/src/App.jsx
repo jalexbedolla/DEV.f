370b6077aebe06476b57051e7a941afc30e4618e
alexBedolla <j.alexbedolla.a@gmail.com>
2022-12-01 14:35:58 -0700

    copiado del repo

---
+++ kataFrontend/giphy-api/src/App.jsx
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
