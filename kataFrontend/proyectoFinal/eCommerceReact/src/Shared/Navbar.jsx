0dc1bf3c15f722690302f4717025ac0de5374609
alexBedolla <j.alexbedolla.a@gmail.com>
2022-12-24 00:07:58 -0700

    react router dom practice init

--- kataFrontend/proyectoFinal/eCommerceReact/src/Shared/Navbar.jsx
+++ kataFrontend/proyectoFinal/eCommerceReact/src/Shared/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <div className='flex space-x-4 bg-white h-[74px] shadow-lg text-center justify-between items-center px-4'>
-      <Link to='/'>Home</Link>
+      <Link to='/' className='text-orange-500'>Home</Link>
     </div>
   )
 }
