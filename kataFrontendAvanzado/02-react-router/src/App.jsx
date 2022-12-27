12dfd1e0ad8941a2f1acb5500e445dba26df250b
alexBedolla <j.alexbedolla.a@gmail.com>
2022-12-26 19:14:07 -0700

    rutas dinamicas y anidadas, useNavigate

--- kataFrontendAvanzado/02-react-router/src/App.jsx
+++ kataFrontendAvanzado/02-react-router/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, Link, useLocation } from 'react-router-dom'
+import { Routes, Route, Link, useLocation, useParams, Outlet, useNavigate } from 'react-router-dom'
 
 function App () {
   return (
@@ -19,7 +19,9 @@ function App () {
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/portafolio' element={<Portafolio />} />
+        <Route path='/portafolio' element={<Portafolio />}>
+          <Route path=':pid' element={<PortafolioDetalle />} />
+        </Route>
         <Route path='/contacto' element={<Contacto />} />
         <Route path='*' element={<Error404 />} />
       </Routes>
@@ -44,7 +46,48 @@ function Portafolio () {
   return (
     <div>
       <h1>Portafolio</h1>
+      <ul>
+        <li>
+          <Link to='/portafolio/1'>Pinterest</Link>
+        </li>
+        <li>
+          <Link to='/portafolio/2'>ATM</Link>
+        </li>
+        <li>
+          <Link to='/portafolio/3'>PokeDex</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
+  )
+}
+
+/* PORTAFOLIO DETALLE */
+function PortafolioDetalle () {
+  const proyectos = [
+    { id: 1, nombre: 'Pinterest', desc: 'clon de pinterest con HTML y CSS' },
+    { id: 2, nombre: 'ATM', desc: 'Simular un cajero automatico con HTML CSS y JS' },
+    { id: 3, nombre: 'PokeDex', desc: 'consumir la PokeApi y mostrar un listado de pokemones con HTML CSS y JS' }
+  ]
+
+  /* useParam me permite leer los params de mis rutas para tener rutas dinamicas */
+  const { pid } = useParams()
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <h3>ID: {proyectos[pid - 1].id}</h3>
+      <h3>Nombre: {proyectos[pid - 1].nombre}</h3>
+      <h3>Descripcion: {proyectos[pid - 1].desc}</h3>
+
+      <button onClick={() => {
+        navigate('/portafolio')
+        /* truco con el navigate. si quiero ir a la paguina anterior uso navigate(-1) y navigate(1) para el forward */
+      }}
+      >
+        Portafolio
+      </button>
+    </>
   )
 }
 
